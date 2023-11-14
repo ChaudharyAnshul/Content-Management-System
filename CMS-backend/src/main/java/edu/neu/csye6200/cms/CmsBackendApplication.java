32db68b1d6d66ad0c32665e2127f7806b9566ede
ChaudharyAnshul <144816995+ChaudharyAnshul@users.noreply.github.com>
2023-11-14 11:45:45 -0500

    Spring Boot Application Initial Commit

---
+++ CMS-backend/src/main/java/edu/neu/csye6200/cms/CmsBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.neu.csye6200.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsBackendApplication.class, args);
+    }
+
+}
---
+++ CMS-backend/src/test/java/edu/neu/csye6200/cms/CmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.neu.csye6200.cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
